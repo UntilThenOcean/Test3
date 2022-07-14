c6c5c22a9b5cc7681f9e2af554b0d90fd9b911d7
UntilThenOcean <juuurihh@gmail.com>
2022-07-14 19:15:29 +0800

    Initial commit

---
+++ src/com/company/One.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.*;
+
+public class One {
+
+    public static void main(String[] args) {
+
+        String meeting = "Hello";
+        String word  = "World";
+/*
+        StringBuilder builder = new StringBuilder();
+        builder.append(meeting);
+        builder.append(word);
+        String completedString = builder.toString();
+        System.out.println(completedString);
+*/
+ /*       Scanner in = new Scanner(System.in);
+        System.out.print("What is your name ? ");
+        String name = in.nextLine();
+
+
+        System.out.println("How old are you?");
+        int age = in.nextInt();
+        System.out.println(name+age);
+*/
+
+       // System.out.printf("%tc", new Date());
+
+/*        Scanner inTwo = new Scanner(System.in);
+
+        System.out.println("Select an option (1,2,3,4)?");
+        int k = inTwo.nextInt();
+
+        switch (k) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                break;
+        }*/
+
+        int[] a = new int[100];
+/*
+        // they are the same:
+        for (int i = 0; i<a.length;i++) {
+
+            System.out.println(a[i]);
+        }
+        // for each
+        for (int element:a) {
+            System.out.println(element);
+        }*/
+        System.out.println(Arrays.toString(a));
+
+
+
+
+
+    }
+}
